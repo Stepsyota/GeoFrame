a207a1cad5d0b89c538fd1017b99976460d61bfc
Stepan <stepsyota@gmail.com>
2025-08-11 23:30:49 +0300

    Add: bot start, handlers, logging, DB init

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from config import TOKEN
 
+from telegram.ext import ApplicationBuilder
+from bot.handlers import register_handlers
+
+import logging
+from logger import setup_logger
+
+from db import init_db
+
 def main():
-    pass
+    setup_logger()
+    logging.info("BOT STARTED")
+    app = ApplicationBuilder().token(TOKEN).build()
+    register_handlers(app)
+    app.run_polling()
 
 if __name__ == '__main__':
+    init_db()
     main()
