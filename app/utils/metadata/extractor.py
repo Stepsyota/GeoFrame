5403d485410900443c8ae0f4f821a3a3b4429f50
Stepan <stepsyota@gmail.com>
2026-02-02 16:36:19 +0300

    FIX: changed filename in import

--- app/utils/metadata/extractor.py
+++ app/utils/metadata/extractor.py
@@ -3,7 +3,7 @@ from app.utils.metadata.models import MediaMetadata
 from app.utils.metadata.common import extract_common_meta
 from app.utils.metadata.image import extract_photo_meta
 from app.utils.metadata.video import extract_video_meta
-from app.utils.check_file import is_image, is_video
+from app.utils.file_type import is_image, is_video
 
 def extract_meta(path : Path) -> MediaMetadata:
     common = extract_common_meta(path)
