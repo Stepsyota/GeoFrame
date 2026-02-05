46091f78f8ab954f4ee080db718db67b9131dcc4
Stepan <stepsyota@gmail.com>
2026-02-05 13:19:46 +0300

    ADD: main crud for media

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,16 +1,13 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.tables import MediaFile, MediaType, MediaImage, MediaVideo
+from sqlalchemy.orm import joinedload
+
+from app.db.tables import MediaFile, MediaType, MediaImage, MediaVideo, MediaLivePhoto
 from app.utils.metadata.models import MediaMetadata
 
 
-async def get_files(session : AsyncSession) -> list[MediaFile]:
-    result = await session.execute(
-        select(MediaFile)
-    )
-    return result.scalars().all()
-
-async def create_file(session : AsyncSession, metadata : MediaMetadata) -> MediaFile:
+# MediaFile
+async def create_mediafile(session : AsyncSession, metadata : MediaMetadata) -> MediaFile:
     media = MediaFile(
         path=str(metadata.path.parent),
         filename=metadata.filename,
@@ -23,27 +20,52 @@ async def create_file(session : AsyncSession, metadata : MediaMetadata) -> Media
         gps_lat=metadata.gps_lat,
         gps_lon=metadata.gps_lon,
         altitude=metadata.altitude,
-        media_type=(
-            MediaType.video if metadata.duration is not None
-            else MediaType.image
-
-        )
     )
-    session.add(media)
-    await session.flush()
 
     if metadata.phash is not None:
-        session.add(
-            MediaImage(
-                media_id = media.id,
-                phash = metadata.phash
-            )
-        )
-    if metadata.duration is not None:
-        session.add(
-            MediaVideo(
-                media_id = media.id,
-                duration = metadata.duration
-            )
-        )
+        media.image = MediaImage(phash=metadata.phash)
+        media.media_type = MediaType.image
+    elif metadata.duration is not None:
+        media.video = MediaVideo(duration=metadata.duration)
+        media.media_type = MediaType.video
+    else:
+        raise ValueError("Cannot determine media type: no phash and no duration")
+
+    session.add(media)
+    await session.flush()
+    await session.refresh(media, ["image", "video"])
     return media
+
+
+async def get_mediafile(session : AsyncSession, id : int) -> MediaFile:
+    result = await session.execute(
+        select(MediaFile)
+        .options(
+            joinedload(MediaFile.image),
+            joinedload(MediaFile.video)
+        )
+        .where(MediaFile.id == id)
+    )
+    return result.scalar_one_or_none()
+
+async def get_mediafiles(session : AsyncSession) -> list[MediaFile]:
+    result = await session.execute(
+        select(MediaFile)
+    )
+    return result.scalars().all()
+
+
+#MediaLivePhoto
+async def create_live_photo(session : AsyncSession, image_id : int, video_id : int):
+    live_photo = MediaLivePhoto(image_media_id=image_id, video_media_id=video_id)
+    session.add(live_photo)
+    await session.flush()
+    return live_photo
+
+async def get_live_photo(session : AsyncSession, live_photo_id : int):
+    result = await session.execute(
+        select(MediaLivePhoto)
+        .options(joinedload(MediaLivePhoto.image_media_file), joinedload(MediaLivePhoto.video_media_file))
+        .where(MediaLivePhoto.id == live_photo_id)
+    )
+    return result.scalar_one_or_none()
