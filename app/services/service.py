2b97ab90b71ac36205dba6dc5347ef4a54ffee52
Stepan <stepsyota@gmail.com>
2026-02-08 22:27:26 +0300

    FIX: names of functions

--- app/services/service.py
+++ app/services/service.py
@@ -1,8 +1,8 @@
 from app.db.database import async_session_factory
-from app.db.tables import MediaFile
+from app.db.tables_media import MediaFile
 from app.utils.scanner import scan_directory
 from app.config import PHOTOS_DIR
-from app.db.crud import create_file, get_files
+from app.db.crud_media import create_mediafile, get_mediafiles
 from app.utils.metadata.extractor import extract_meta
 import asyncio
 from pathlib import Path
@@ -11,7 +11,7 @@ from pathlib import Path
 async def add_file_to_db(file : Path):
     async with async_session_factory() as session:
         async with session.begin():
-            await create_file(session, extract_meta(file))
+            await create_mediafile(session, extract_meta(file))
 
 async def add_files_to_db():
     files = scan_directory(PHOTOS_DIR)
@@ -20,10 +20,10 @@ async def add_files_to_db():
 
 async def get_files_from_db() -> list[MediaFile]:
     async with async_session_factory() as session:
-        files = await get_files(session)
+        files = await get_mediafiles(session)
         return files
 
-#asyncio.run(add_files_to_db())
+asyncio.run(add_files_to_db())
 #print(asyncio.run(get_files_from_db()))
 
 
